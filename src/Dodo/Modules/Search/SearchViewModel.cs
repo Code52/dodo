251ba1647393d329373408a0cf106242d51d49c1
Brendan Forster <me@brendanforster.com>
2012-04-10 20:35:16 -0700

    wired up search UI

--- src/Dodo/Modules/Search/SearchViewModel.cs
+++ src/Dodo/Modules/Search/SearchViewModel.cs
@@ -4,5 +4,11 @@ namespace Dodo.Modules.Search
 {
     public class SearchViewModel : BindableBase
     {
+        private string _searchText;
+        public string SearchText
+        {
+            get { return _searchText; }
+            set { SetProperty(ref _searchText, value); }
+        }
     }
 }
