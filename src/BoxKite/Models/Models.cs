864c4c720a872cdb7d0d3fd6f6e4b514ff55c76f
Paul Jenkins <paul@theleagueofpaul.com>
2012-04-14 11:02:38 +1000

    Added DirectMessages

--- src/BoxKite/Models/Models.cs
+++ src/BoxKite/Models/Models.cs
@@ -166,5 +166,20 @@ namespace BoxKite.Models
         public bool retweeted { get; set; }
         public bool? possibly_sensitive { get; set; }
     }
+
+    public class DM
+    {
+        public string created_at { get; set; }
+        public string sender_screen_name { get; set; }
+        public User sender { get; set; }
+        public string text { get; set; }
+        public string recipient_screen_name { get; set; }
+        public long id { get; set; }
+        public User recipient { get; set; }
+        public long recipient_id { get; set; }
+        public long sender_id { get; set; }
+    }
     // ReSharper restore InconsistentNaming
 }
+
+
